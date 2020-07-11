e5bfcc27642ea57cc522859525e8db669b085bab
Michael Fahim <michael_fahim@yahoo.com>
2020-07-11 19:25:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get "/reversename/:name" do
+     @user_name = params[:name]
+     @reversed_string = user_name.reverse
+     erb :reversed
 
+   end
 end
